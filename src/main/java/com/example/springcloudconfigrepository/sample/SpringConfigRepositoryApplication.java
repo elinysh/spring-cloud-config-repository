927fa7114232b55bbeb73600ba020f65d62e8bbc
user <36792>
2021-03-30 00:30:11 +0900

    repository 세팅

---
+++ src/main/java/com/example/springcloudconfigrepository/sample/SpringConfigRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigrepository.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcloudconfigrepository/sample/SpringConfigRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigrepository.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
